0a38a9c1edfd2f54fdb678fd8c0603e556869370
Dmsrmdni <dimasramdani220@gmail.com>
2022-03-27 19:36:21 +0700

    Menambahkan table Pengguna,Pegawai dan Tamu

---
+++ database/migrations/2022_03_27_123238_create_tamus_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tamus', function (Blueprint $table) {
+            $table->id();
+            $table->string("nama_pemesan");
+            $table->string("jenis_kamar");
+            $table->integer("lama_pesanan");
+            $table->date("tanggal_pesanan");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tamus');
+    }
+};
